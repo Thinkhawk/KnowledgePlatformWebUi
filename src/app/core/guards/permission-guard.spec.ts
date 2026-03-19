ff32a01b7a580b63e7e1d7dfe300762c3f60589a
Nidhipriya.Bandaru <Nidhipriya.Bandaru@gmail.com>
2026-03-19 16:12:18 +0530

    structure initial changes

---
+++ src/app/core/guards/permission-guard.spec.ts
@@ -0,0 +1,3 @@
+export const permissionGuard = () => {
+  return true;
+};
--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -1,1 +1,9 @@
-// blank
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  [x: string]: any;
+  isLoading = false;
+}
