cfe9413fa5a058ec759e260f12393f027b36d9bb
Nidhipriya.Bandaru <Nidhipriya.Bandaru@gmail.com>
2026-03-23 10:41:41 +0530

    Updated - loading.interceptor, loading.service, app.ts, main.ts,package.json

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
-import { AppComponent } from './app/app';
+import { App } from './app/app';
 import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
+bootstrapApplication(App, {
   providers: [
     provideRouter(routes)
   ]
